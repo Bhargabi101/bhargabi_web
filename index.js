162205046953c96eb28795de95c0a57ecf255d74
Bhargabi101 <166243859+Bhargabi101@users.noreply.github.com>
2025-07-19 21:01:32 +0530

    add take some rest and nice meeting u

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log("hello world 2")
 console.log("my name is Bhargabi")
 
 console.log("iam pursuing Bsc ITM")
+
+console.log("its a great meeting u")
--- script.js
+++ script.js
@@ -3,3 +3,5 @@ console.log("hello world")
 console.log("hi how u doing")
 
 console.log("have u eaten something ?")
+
+console.log("take some rest the get back to work")
